b5a849116ca16fed1214345dfeab6ae7ffe427cc
Cedric Gasser <cedigasser@gmail.com>
2021-11-21 23:08:40 +0100

    Change under construciton style with help from @Chaliil

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>This site is under construction!</h1>
-<p>You can show up later at some poin.</p>
+<script>
+
+</script>
+
+<main>
+    <div>
+        <h1>This site is under construction!</h1>
+        <p>You can show up later at some poin.</p>
+    </div>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+    * {
+        font-family: Nunito, Roboto, system-ui, sans-serif;;
+    }
+    main {
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        color: #D9CB6B;
+    }
+</style>
