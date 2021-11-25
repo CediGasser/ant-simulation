a82925710fcafbfec4a24f82388533f7de5ab72f
Cedric Gasser <58805568+CediGasser@users.noreply.github.com>
2021-11-25 17:38:42 +0100

    Fix typo, thx to @Lenoxy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 <main>
     <div>
         <h1>This site is under construction!</h1>
-        <p>You can show up later at some poin.</p>
+        <p>You can show up later at some point.</p>
     </div>
 </main>
 
