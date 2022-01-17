cea329c604e3031a9a7b2f3f6773ce705c2065cf
Cedric Gasser <cedigasser@gmail.com>
2022-01-17 07:47:57 +0100

    Add benchmark input values

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,19 +3,14 @@
     import Simulation from "../components/Simulation.svelte"
     
     let antCount = 100
+    let antSize = 1
 </script>
 
 <main>
     <div>
-        <Simulation {antCount} antSize={5}></Simulation>
-        <input type="range" bind:value={antCount}><input/>
+        <Simulation {antCount} {antSize}></Simulation>
+        <input type="number" bind:value={antCount} max="20000" min="0"/>
         <Slider></Slider>
-    </div>
-    <div>
-        <h1>This site is under construction!</h1>
-        <p>You may show up later at some point.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
-        </p>
     </div>
 </main>
 
