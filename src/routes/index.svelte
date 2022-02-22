4ea8ad214fb467cade924f2075c056a692f954c2
Cedric Gasser <cedigasser@gmail.com>
2022-02-22 18:45:34 +0100

    Add Seo.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,14 @@
 <script>
-    import Simulation from "../components/Simulation.svelte"
-    import PerformanceMeter from "../components/PerformanceMeter.svelte"
+    import Simulation from "$lib/components/Simulation.svelte"
+    import PerformanceMeter from "$lib/components/PerformanceMeter.svelte"
+    import Seo from '$lib/components/Seo.svelte'
 
     let antCount = 100
     let antSize = 1
     let playing = false
 </script>
+
+<Seo title="Ant Simulation" keywords="Ants Simulation" type="Website" description="An interactive ant simulation. Thats realy about it. Oh, and we also made a paper about the stuff we learned." image="/social_preview.png"/>
 
 <main>
     <div>
